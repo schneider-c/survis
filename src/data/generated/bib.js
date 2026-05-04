1afd89ceb6881939a45645280f9bccc71e1dcd9e
schneider-c <80567103+schneider-c@users.noreply.github.com>
2026-05-04 14:02:33 +0800

    Add files via upload

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,134 @@
 ﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+    "agarwal2025skincancerkan": {
+        "author": "Agarwal, Shubhi and Mahto, Amulya Kumar",
+        "doi": "10.48550/arXiv.2508.12484",
+        "journal": "arXiv preprint arXiv:2508.12484",
+        "keywords": "kan-medical, cnn-transformer, skin-cancer-classification, feature-fusion, medical-image-classification",
+        "title": "Skin Cancer Classification: Hybrid CNN-Transformer Models with KAN-Based Fusion",
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
+        "year": "2025"
+    },
+    "aghaei2024rkan": {
+        "author": "Aghaei, Alireza Afzal",
+        "doi": "10.48550/arXiv.2406.14495",
+        "journal": "arXiv preprint arXiv:2406.14495",
+        "keywords": "kan-variant, rational-functions, kolmogorov-arnold-network",
+        "title": "rKAN: Rational Kolmogorov-Arnold Networks",
+        "type": "article",
+        "year": "2024"
+    },
+    "halder2024vit": {
+        "author": "Halder, Arindam and Gharami, Sanghita and Sadhu, Priyangshu and Singh, Pawan Kumar and Wozniak, Marcin and Ijaz, Muhammad Fazal",
+        "doi": "10.1038/s41598-024-63094-9",
+        "journal": "Scientific Reports",
+        "keywords": "vision-transformer, biomedical-image-classification, medmnist, medical-image-classification",
+        "number": "1",
+        "pages": "12490",
+        "publisher": "Nature Publishing Group",
+        "title": "Implementing Vision Transformer for Classifying 2D Biomedical Images",
+        "type": "article",
+        "volume": "14",
+        "year": "2024"
+    },
+    "he2026vkt": {
+        "author": "He, Guanghua and Fu, Yuanyuan and Cheng, Wangang",
+        "doi": "10.1002/ima.70334",
+        "journal": "International Journal of Imaging Systems and Technology",
+        "keywords": "kan-medical, kan-transformer, hybrid-architecture, medical-image-classification",
+        "number": "2",
+        "publisher": "Wiley",
+        "title": "VKT: A Hybrid Vision KAN-Transformer Model for Medical Image Classification",
+        "type": "article",
+        "volume": "36",
+        "year": "2026"
+    },
+    "hou2024criticalkan": {
+        "author": "Hou, Yuntian and Ji, Tianrui and Zhang, Di and Stefanidis, Angelos",
+        "doi": "10.48550/arXiv.2407.11075",
+        "journal": "arXiv preprint arXiv:2407.11075",
+        "keywords": "kan-limitation, critical-assessment, practical-viability, neural-architecture",
+        "title": "Kolmogorov-Arnold Networks: A Critical Assessment of Claims, Performance, and Practical Viability",
+        "type": "article",
+        "year": "2024"
+    },
+    "liu2024kan": {
+        "author": "Liu, Ziming and Wang, Yixuan and Vaidya, Sachin and Ruehle, Fabian and Halverson, James and Solja{\\v{c}}i{\\'c}, Marin and Hou, Thomas Y. and Tegmark, Max",
+        "doi": "10.48550/arXiv.2404.19756",
+        "journal": "arXiv preprint arXiv:2404.19756",
+        "keywords": "kan-theory, kolmogorov-arnold-network, neural-architecture",
+        "title": "KAN: Kolmogorov-Arnold Networks",
+        "type": "article",
+        "year": "2024"
+    },
+    "manzari2023medvit": {
+        "author": "Manzari, Omid Nejati and Ahmadabadi, Hamid and Kashiani, Hossein and Shokouhi, Shahriar B. and Ayatollahi, Ahmad",
+        "doi": "10.1016/j.compbiomed.2023.106791",
+        "journal": "Computers in Biology and Medicine",
+        "keywords": "cnn-transformer, vision-transformer, medical-image-classification, robustness",
+        "pages": "106791",
+        "publisher": "Elsevier",
+        "title": "MedViT: A Robust Vision Transformer for Generalized Medical Image Classification",
+        "type": "article",
+        "volume": "157",
+        "year": "2023"
+    },
+    "manzari2026medvitv2": {
+        "author": "Manzari, Omid Nejati and Asgariandehkordi, Hojat and Koleilat, Taha and Xiao, Yiming and Rivaz, Hassan",
+        "doi": "10.1016/j.asoc.2025.114045",
+        "journal": "Applied Soft Computing",
+        "keywords": "kan-medical, kan-transformer, vision-transformer, medical-image-classification, robustness",
+        "pages": "114045",
+        "publisher": "Elsevier",
+        "title": "Medical Image Classification with KAN-Integrated Transformers and Dilated Neighborhood Attention",
+        "type": "article",
+        "volume": "186",
+        "year": "2026"
+    },
+    "pan2026medvk": {
+        "author": "Pan, Weichao and Wang, Xu",
+        "doi": "10.1016/j.patrec.2026.03.004",
+        "journal": "Pattern Recognition Letters",
+        "keywords": "kan-medical, vision-kan, efficiency, medical-image-classification",
+        "pages": "152--159",
+        "publisher": "Elsevier",
+        "title": "MedVK: Efficient Medical Image Classification via Decoupled Vision Kolmogorov-Arnold Networks",
+        "type": "article",
+        "volume": "203",
+        "year": "2026"
+    },
+    "shamshad2023transformers": {
+        "author": "Shamshad, Fahad and Khan, Salman and Zamir, Syed Waqas and Khan, Muhammad Haris and Hayat, Munawar and Khan, Fahad Shahbaz and Fu, Huazhu",
+        "doi": "10.1016/j.media.2023.102802",
+        "journal": "Medical Image Analysis",
+        "keywords": "survey, vision-transformer, medical-imaging, medical-image-classification",
+        "pages": "102802",
+        "publisher": "Elsevier",
+        "title": "Transformers in Medical Imaging: A Survey",
+        "type": "article",
+        "volume": "88",
+        "year": "2023"
+    },
+    "yang2023medmnist": {
+        "author": "Yang, Jiancheng and Shi, Rui and Wei, Donglai and Liu, Zequan and Zhao, Lin and Ke, Bilian and Pfister, Hanspeter and Ni, Bingbing",
+        "doi": "10.1038/s41597-022-01721-8",
+        "journal": "Scientific Data",
+        "keywords": "benchmark, dataset, medical-image-classification",
+        "number": "1",
+        "pages": "41",
+        "publisher": "Nature Publishing Group",
+        "title": "MedMNIST v2: A Large-Scale Lightweight Benchmark for 2D and 3D Biomedical Image Classification",
+        "type": "article",
+        "volume": "10",
+        "year": "2023"
+    },
+    "yang2025medkan": {
+        "author": "Yang, Zhuoqin and Zhang, Jiansong and Luo, Xiaoling and Wu, Xu and Lu, Zheng and Shen, Linlin",
+        "booktitle": "2025 IEEE International Conference on Bioinformatics and Biomedicine",
+        "doi": "10.1109/BIBM66473.2025.11356561",
+        "keywords": "kan-medical, medical-image-classification, kolmogorov-arnold-network, hybrid-architecture",
+        "publisher": "IEEE",
+        "title": "MedKAN: An Advanced Kolmogorov-Arnold Network for Medical Image Classification",
+        "type": "inproceedings",
+        "year": "2025"
     }
 };
